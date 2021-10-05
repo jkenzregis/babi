e9f25cbe6464e4210357aadf441633e53963e1dd
jkenzregis <73495792+jkenzregis@users.noreply.github.com>
2021-10-05 11:58:31 -0700

    Add files via upload

---
+++ admin/import_teacher.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	</head>
+	
+<body>
+
+	<div class="col-md-3"></div>
+	<div class="col-md-6 well">
+		<h3 class = "text-primary">PHP - Import CSV File Into MySQLi Database</h3>
+		<hr style = "border-top:1px dotted #ccc;"/>
+		<div class="col-md-4" style='overflow:hidden;'>
+			<form action="upload2.php" class="form-inline" method="POST" enctype="multipart/form-data">
+				<div class="form-group">
+					<label>CSV here:</label>
+					<br />
+					<input type="file" name="file"/>
+					<br />
+					<center><button name="upload" class="btn btn-primary"><span class="glyphicon glyphicon-upload"></span> Upload</button></center>
+				</div>
+			</form>
+		</div>
+		<div class="col-md-8">
+			<table class="table table-bordered">
+				<thead class="alert-info">
+					<tr>
+					    <th>School Id</th>
+						<th>Firstname</th>
+						<th>Lastname</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						require'conn.php';
+						$query=mysqli_query($conn, "SELECT * FROM `faculty_list`") or die(mysqli_error());
+						while($fetch=mysqli_fetch_array($query)){
+					?>
+						<tr>
+							<td><?php echo $fetch['school_id']?></td>
+							<td><?php echo $fetch['firstname']?></td>
+							<td><?php echo $fetch['lastname']?></td>
+						</tr>
+					<?php
+						}
+					?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
